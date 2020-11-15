2df85018c5bb859888f19a4410fcce76c5060abc
Julian Andrews <jandrews@fusionbox.com>
2020-11-14 21:21:40 -0500

    Add goban range parsing
    And this finishes up the initially planned features!
    
    Markup support will be needed to make this a serious tool, but it's a
    pretty good start!

--- src/args.rs
+++ src/args.rs
@@ -8,9 +8,6 @@ pub fn parse_args(
     opts: &getopts::Options,
     args: &Vec<String>,
 ) -> Result<SgfRenderArgs, UsageError> {
-    // TODO: Parse goban_range somehow.
-    let goban_range = GobanRange::ShrinkWrap;
-
     let matches = opts
         .parse(&args[1..])
         .map_err(|_| UsageError::FailedToParse)?;
@@ -31,6 +28,20 @@ pub fn parse_args(
         .unwrap_or(Ok(DEFAULT_WIDTH))
         .map_err(|_| UsageError::InvalidWidth)? as f64;
     let print_help = matches.opt_present("h");
+    let goban_range = {
+        if matches.opt_present("shrink-wrap") && matches.opt_present("r") {
+            return Err(UsageError::OverspecifiedRange);
+        }
+
+        if matches.opt_present("shrink-wrap") {
+            GobanRange::ShrinkWrap
+        } else {
+            match matches.opt_str("r") {
+                Some(s) => parse_point_pair(&s)?,
+                None => GobanRange::FullBoard,
+            }
+        }
+    };
 
     let options = MakeSvgOptions {
         goban_range,
@@ -75,6 +86,17 @@ pub fn build_opts() -> getopts::Options {
         ),
         "WIDTH",
     );
+    opts.optflag(
+        "s",
+        "shrink-wrap",
+        "Draw only enough of the board to hold all the stones (with 1 space padding)",
+    );
+    opts.optopt(
+        "r",
+        "range",
+        "Range to draw as a pair of corners (e.g. 'cc-ff')",
+        "RANGE",
+    );
     opts.optflag("", "no-labels", "Don't render labels on the diagram");
     opts.optflag("h", "help", "Display this help and exit");
 
@@ -96,6 +118,8 @@ pub enum UsageError {
     TooManyArguments,
     InvalidMoveNumber,
     InvalidWidth,
+    OverspecifiedRange,
+    InvalidRange,
 }
 
 impl std::fmt::Display for UsageError {
@@ -105,8 +129,29 @@ impl std::fmt::Display for UsageError {
             UsageError::TooManyArguments => write!(f, "Too many arguments."),
             UsageError::InvalidMoveNumber => write!(f, "Invalid move number."),
             UsageError::InvalidWidth => write!(f, "Invalid width."),
+            UsageError::OverspecifiedRange => write!(f, "Specify only '-r' or '-s'"),
+            UsageError::InvalidRange => write!(f, "Invalid range."),
         }
     }
 }
 
 impl ::std::error::Error for UsageError {}
+
+fn parse_point_pair(s: &str) -> Result<GobanRange, UsageError> {
+    let parse_byte = |b: u8| {
+        if b < b'a' || b > b'z' {
+            Err(UsageError::InvalidRange)
+        } else {
+            Ok(b - b'a')
+        }
+    };
+
+    let s = s.as_bytes();
+    if s.len() != 5 || s[2] != b'-' {
+        return Err(UsageError::InvalidRange);
+    }
+    Ok(GobanRange::Ranged(
+        parse_byte(s[0])?..parse_byte(s[3])? + 1,
+        parse_byte(s[1])?..parse_byte(s[4])? + 1,
+    ))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(str_split_once)]
+
 mod args;
 mod lib;
 
