493c03c84c4edae1b6da6305143c12239edf5f0b
Julian Andrews <jandrews271@gmail.com>
2021-09-05 21:58:49 -0400

    Shrink main demo image.

--- generate-demos.sh
+++ generate-demos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cargo run -- tests/data/prob45/input.sgf --shrink-wrap -o demo/prob45.svg
+cargo run -- tests/data/prob45/input.sgf --shrink-wrap --width 400 -o demo/prob45.svg
 cargo run -- tests/data/prob45/input.sgf --shrink-wrap -o demo/prob45.png
 cargo run -- tests/data/prob45/input.sgf --shrink-wrap --style fancy -o demo/prob45-fancy.svg
 cargo run -- tests/data/markup/input.sgf -n 2 --range cc-qk -o demo/markup.svg
